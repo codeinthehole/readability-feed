d98151854062a5c535378fba7a149a987b1f885e
David Winterbottom <david.winterbottom@gmail.com>
2012-09-23 16:11:07 +0100

    First working version

---
+++ update.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# Config
+import logging
+import requests
+import re
+import os
+import readability
+
+urlfinder = re.compile(r"(https?://[^ )]+)")
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+filepath = os.path.join(os.path.dirname(__file__), 'readability.log')
+fh = logging.FileHandler(filepath)
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def get_article_urls_from_twitter_favourites(username, n=20):
+    """
+    Return a list of article URLs extracted from a user's twitter favourites
+    """
+    urls = []
+
+    logger.info("Fetching Twitter favourite articles for %s", username)
+    twitter_url = 'https://api.twitter.com/1/favorites.json?count=%d&screen_name=%s' % (
+        n, username)
+    response = requests.get(twitter_url)
+
+    if response.status_code != 200:
+        return []
+
+    for tweet in response.json:
+        text = tweet['text']
+        # Look for a link
+        match = urlfinder.search(text)
+        if not match:
+            continue
+        # Check link is an article
+        redirect_url = match.groups()[0]
+        url_resp = requests.get(redirect_url)
+        if 'text/html' not in url_resp.headers['content-type']:
+            continue
+        url = url_resp.url
+        if url.endswith('.jpg'):
+            continue
+        if 'vimeo' in url:
+            continue
+        urls.append(url_resp.url)
+
+    logger.info("Found %d articles", len(urls))
+    return urls
+
+from config import *
+
+token = readability.xauth(
+    CONSUMER_KEY, CONSUMER_SECRET, USERNAME, PASSWORD)
+rdd = readability.oauth(
+    CONSUMER_KEY, CONSUMER_SECRET, token=token)
+
+user = rdd.get_me()
+
+logger.info("Updating readability library")
+
+logger.info("Fetching library")
+library_urls = [u.article.url for u in user.bookmarks()]
+logger.info("Found %d articles in library", len(library_urls))
+
+urls = get_article_urls_from_twitter_favourites(TWITTER_USERNAME)
+
+num_dupes = num_new = 0
+for url in urls:
+    if url in library_urls:
+        num_dupes += 1
+    else:
+        num_new += 1
+        rdd.add_bookmark(url)
+
+logger.info("Added %d new articles, found %d dupes", num_new, num_dupes)
